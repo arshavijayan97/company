48380be0379565dd03e0c7823f85df40836392fc
arshavijayan97 <arshavijayanm97@gmail.com>
2023-10-09 12:01:11 +0530

    final commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,12 +9,7 @@ class UserController extends Controller
 {
     public function getUserDetails(Request $request)
     {
-        $user = $request->user(); // Retrieve the authenticated user
-        return response()->json([
-            'id' => $user->id,
-            'name' => $user->name,
-            'email' => $user->email,
-            'role' => $user->role,
-        ]);
+        
+    return response()->json(['user' => auth()->user()], 200);
     }
 }
